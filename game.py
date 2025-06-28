58af1b841482270aeae6d07e1eea5068c2c6e612
RoeunApireak <apireak.roeun.datau@gmail.com>
2025-06-28 22:08:20 +0700

    minecraft day1

---
+++ game.py
@@ -0,0 +1,16 @@
+#write here a code for main window of the game
+from direct.showbase.ShowBase import ShowBase
+from mapmanager import Mapmanager
+
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.land = Mapmanager()
+        self.land.loadLand("land.txt")
+
+        base.camLens.setFov(90)
+
+game = Game()
+game.run()
+
