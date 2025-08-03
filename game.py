d5fb0834898b6d03d716fe2db0764704a158d740
Tvsterb1 <145824995+Tvsterb1@users.noreply.github.com>
2025-08-03 14:15:21 +0700

    Mypatchly

--- game.py
+++ game.py
@@ -1,16 +1,13 @@
+# Starts up the game
 from direct.showbase.ShowBase import ShowBase
-
-from mapmanager import Mapmanager
-
-
+from mapmanager import mapmanager
+from hero import Hero
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        
-        self.land = Mapmanager('block.egg',
-                               'block.png')
-        self.land.loadLand('land.txt')
-
+        self.land = mapmanager() #create a map
+        self.land.loadLand("land2.txt")
+        self.hero = Hero(pos=(x//2, y//2, 2), land=self.land)
         base.camLens.setFov(90)
 
 game = Game()
